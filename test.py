16f18e46d8a3e1a76e628a04c60650bcff083eb9
coding-fantastic <alexnzioka@protonmail.com>
2020-04-24 23:33:16 +0300

    populating table

--- test.py
+++ test.py
@@ -1,13 +1,19 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-host = 'localhost',
-user = 'alex',
-password = '',
+host = "localhost",
+user = "alex",
+password = "",
 database="testdb"
 )
 mycursor = mydb.cursor()
-mycursor.execute("create table students (name varchar(255), age integer(10))")
+sqlFormula = "insert into students (name , age )values (%s, %s)"
 
-for db in mycursor:
-    print(db)
+students = [("lamken" , 22),
+                ("puffy", 24),
+                ("Jacob", 12),
+                ("Randomgee", 25),
+                ("Yvonne", 20)]
+
+mycursor.execute(sqlFormula, students);
+mydb.commit();
