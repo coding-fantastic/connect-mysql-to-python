63ef7d118440bc586c3ae5af7c5ea049a988bddc
coding-fantastic <alexnzioka@protonmail.com>
2020-04-25 15:05:03 +0300

    tkinter hello world

--- test.py
+++ test.py
@@ -1,13 +1,28 @@
+from tkinter import *
 import mysql.connector
 
-mydb = mysql.connector.connect(
-host = "localhost",
-user = "alex",
-password = "",
-database="testdb"
-)
-mycursor = mydb.cursor()
-mycursor.execute('select * from students where name like "%eff%"' )
-myresult = mycursor.fetchall()
-for row in myresult:
-    print(row)
+root =Tk()
+#creating a label widget
+myLabel =Label(root, text="Hello World!")
+
+#shoving it onto the screen
+myLabel.pack()
+
+#create an event loop
+root.mainloop()
+
+
+def db():
+
+
+    mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "alex",
+    password = "",
+    database="testdb"
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute('select * from students where name like "%eff%"' )
+    myresult = mycursor.fetchall()
+    for row in myresult:
+        print(row)
