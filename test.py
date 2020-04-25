0f029c63d1bfa10988c41f4577f38d50c501d776
coding-fantastic <alexnzioka@protonmail.com>
2020-04-25 20:24:50 +0300

    dispaly data from mysql to gui

--- test.py
+++ test.py
@@ -1,23 +1,6 @@
 from tkinter import *
 import mysql.connector
 
-root =Tk()
-
-e = Entry(root, width = 50)
-e.pack()
-e.insert(0, "Enter you name") #placeholder of textbox 
-
-def myClick():
-    hello = "Hello " + e.get()
-    myLabel = Label(root, text=hello)
-    myLabel.pack()
-myButton = Button(root, text="Enter your name!", padx=20, pady=20, command = myClick)
-myButton.pack()
-
-#create an event loop
-root.mainloop()
-
-
 def db():
 
 
@@ -32,3 +15,26 @@ def db():
     myresult = mycursor.fetchall()
     for row in myresult:
         print(row)
+        myLabel2 =Label(root, text=row)
+        myLabel2.pack()
+
+
+root =Tk()
+
+e = Entry(root, width = 50)
+e.pack()
+
+ #placeholder of textbox
+#e.insert(0, "Enter you name")
+
+def myClick():
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text=hello)
+    myLabel.pack()
+myButton = Button(root, text="Enter your name!", padx=20, pady=20, command = myClick)
+myButton.pack()
+
+myButton = Button(root, text ="Click the button to display all entries in students table" ,command=db )
+myButton.pack()
+#create an event loop
+root.mainloop()
